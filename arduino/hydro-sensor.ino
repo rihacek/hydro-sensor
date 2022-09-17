18fd6583e5f381f1a02e83bdb0e630bfdb2c9fa2
John Rihacek <jrihacek@gmail.com>
2022-09-17 12:13:04 -0400

    clean up Serial/debug calls

--- arduino/hydro-sensor.ino
+++ arduino/hydro-sensor.ino
@@ -7,9 +7,6 @@ const char* mqtt_pw = MQTT_PW;
 #include <WiFiManager.h>
 #include <ESP8266WiFi.h>
 
-// Time
-#include <NTPClient.h>
-
 // MQTT
 #include <PubSubClient.h>
 #include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson/wiki/API%20Reference
@@ -18,13 +15,10 @@ const char* mqtt_pw = MQTT_PW;
 const char* MQTT_CLIENT_ID = "water01";
 const char* MQTT_SERVER_IP = "192.168.86.202";
 const uint16_t MQTT_SERVER_PORT = 1883;
-const char* MQTT_TOPIC = "patio/moisture";
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-const long utcOffsetInSeconds = -18000; //UTC -5.00 (-5 * 60 sec in min * 60 min in hr = -18000)
+const char* MQTT_TOPIC = "moisture01";
 
 WiFiUDP wifi_udp;
 
-NTPClient timeClient(wifi_udp, "pool.ntp.org", utcOffsetInSeconds);
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
 
@@ -69,7 +63,6 @@ void setup() {
     Serial.begin(9600);
     WiFiManager wifiManager;  
     wifiManager.autoConnect();
-    timeClient.begin();
     mqttClient.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
     mqttClient.setCallback(callback);
 } 
@@ -82,20 +75,8 @@ void loop() {
     } 
     sensorValue = sensorValue/100.0; 
 
-    timeClient.update();
-
     if (!mqttClient.connected()) {
         reconnect();
-    }
-
-    int hh = timeClient.getHours();
-    int mm = timeClient.getMinutes();
-    int ss = timeClient.getSeconds();
-
-    bool isPM = false;
-    if(hh > 12) {
-        hh=hh-12;
-        isPM = true;
     }
 
     if (isnan(sensorValue)) {
